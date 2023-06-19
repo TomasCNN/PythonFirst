abd6277e2dca71d8193db7ff2d5118dec2860e2e
Administrator <Administrator@LK-202210260806>
2023-06-19 17:22:22 +0800

    添加项目文件。

---
+++ PythonFirst.py
@@ -0,0 +1,23 @@
+import math
+
+
+a = 3
+print(a)
+b = 3.1412926
+print(math.floor(b))
+
+name = "ada lovelace"
+print(name.title())        # title()以首字母大写的方式显示每个单词，即将每个单词的首字母都改为大写。
+
+print(name.upper())        # upper()将字符串改为全部大写
+
+print(name.lower())        # lower()将字符串改为全部小写
+
+favorite_language = ' python '
+
+print(favorite_language.rstrip())         # rstrip()删除字符串末尾的空白
+
+print(favorite_language.lstrip())         # lstrip()删除字符串开头的空白
+
+print(favorite_language.strip())          # strip()删除字符串两端的空白
+
