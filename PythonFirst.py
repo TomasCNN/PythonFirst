63e210d22cd8cee1dc35c975f48febcfd43f3e03
Tom Tony <Tomason@5n5bg6.onmicrosoft.com>
2023-08-01 17:19:00 +0800

    2023-8-7 11:36:00

--- PythonFirst.py
+++ PythonFirst.py
@@ -47,6 +47,51 @@ print(msg)
 msg = f"Hello, {knames[2].title()}!" 
 print(msg)
 
+'''
+    Python之禅
+    The Zen of Python, by Tim Peters
+
+    Beautiful is better than ugly.
+    Explicit is better than implicit.
+    Simple is better than complex.
+    Complex is better than complicated.
+    Flat is better than nested.
+    Sparse is better than dense.
+    Readability counts.
+    Special cases aren't special enough to break the rules.
+    Although practicality beats purity.
+    Errors should never pass silently.
+    Unless explicitly silenced.
+    In the face of ambiguity, refuse the temptation to guess.
+    There should be one-- and preferably only one --obvious way to do it.
+    Although that way may not be obvious at first unless you're Dutch.
+    Now is better than never.
+    Although never is often better than *right* now.
+    If the implementation is hard to explain, it's a bad idea.
+    If the implementation is easy to explain, it may be a good idea.
+    Namespaces are one honking great idea -- let's do more of those!
+
+        Python程序员笃信代码可以编写得漂亮而优雅。编程是要解决问题的，设计良好、高效而漂
+    亮的解决方案都会让程序员心生敬意。随着你对Python的认识越来越深入，并使用它来编写越来
+    越多的代码，有一天也许会有人站在你后面惊呼：“哇，代码编写得真是漂亮！”
+        如果有两个解决方案，一个简单，一个复杂，但都行之有效，就选择简单的解决方案吧。这
+    样，你编写的代码将更容易维护，你或他人以后改进这些代码时也会更容易。
+        现实是复杂的，有时候可能没有简单的解决方案。在这种情况下，就选择最简单可行的解决
+    方案吧。
+        即便是复杂的代码，也要让它易于理解。开发的项目涉及复杂代码时，一定要为这些代码编
+    写有益的注释。
+        如果让两名Python程序员去解决同一个问题，他们提供的解决方案应大致相同。这并不是说
+    编程没有创意空间，而是恰恰相反！然而，大部分编程工作都是使用常见解决方案来解决简单的
+    小问题，但这些小问题都包含在更庞大、更有创意空间的项目中。在你的程序中，各种具体细节
+    对其他Python程序员来说都应易于理解。
+        你可以将余生都用来学习Python和编程的纷繁难懂之处，但这样你什么项目都完不成。不要
+    企图编写完美无缺的代码；先编写行之有效的代码，再决定是对其做进一步改进，还是转而去编
+    写新代码。
+
+
+'''
+
+
 #'''
 #    f-string，亦称为格式化字符串常量（formatted string literals），
 #    是python3.6新引入的一种字符串格式化方法，该方法源于pep 498 – 
@@ -194,7 +239,202 @@ print(cars)
 
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 print(len(cars))
+print('\t' + str(len(cars)))
 
 #   在你需要完成如下任务时，len()很有用：确定还有多少个外星人未被射杀，需要管理多少项可视化数据，网站有多少注册用户等。
 #   循环这种概念很重要，因为它是让计算机自动完成重复工作的常见方式之一。
+
+#   在for循环中，可对每个元素执行任何操作。
+#   使用for循环处理数据是一种对数据集执行整体操作的不错的方式。
+
+
+#   能使用for循环来初始化游戏——遍历角色列表，将每个角色都显示到屏幕上；再在循环后面添加一个不缩进的代码块，在屏幕上绘制所有角色后显示一个Play Now按钮。
+#   Python根据缩进来判断代码行与前一个代码行的关系。
+
+#   需要存储一组数字的原因有很多，例如，在游戏中，需要跟踪每个角色的位置，还可能需要跟踪玩家的几个最高得分。
+#   在数据可视化中，处理的几乎都是由数字（如温度、距离、人口数量、经度和纬度等）组成的集合。列表非常适合用于存储数字集合。
+
+#   Python函数range()让你能够轻松地生成一系列的数字。
+for value in range(1,5):
+    print('\t' + str(value))
+
+#   使用range()时，如果输出不符合预期，请尝试将指定的值加1或减1。
+
+#   要创建数字列表，可使用函数list()将range()的结果直接转换为列表。
+
+#   列表解析让你只需编写一行代码就能生成这样的列表。列表解析将for循环和创建新元素的代码合并成一行，并自动附加新元素。
+
+#   要创建切片，可指定要使用的第一个元素和最后一个元素的索引。与函数range()一样，Python在到达你指定的第二个索引前面的元素后停止。
+#   要输出列表中的前三个元素，需要指定索引0~3，这将输出分别为0、1和2的元素。
+
+#   在很多情况下，切片都很有用。例如，编写游戏时，你可以在玩家退出游戏时将其最终得分加入到一个列表中。然后，为获取该玩家的三个最高得分，
+#   你可以将该列表按降序排列，再创建一个只包含前三个得分的切片。处理数据时，可使用切片来进行批量处理；编写Web应用程序时，可使用切片来分
+#   页显示信息，并在每页显示数量合适的信息。
+
+#   你经常需要根据既有列表创建全新的列表。下面来介绍复制列表的工作原理，以及复制列表可提供极大帮助的一种情形。
+#   要复制列表，可创建一个包含整个列表的切片，方法是同时省略起始索引和终止索引（[:]）。这让Python创建一个始于第一个元素，终止于最后一个元素的切片，即复制整个列表。
+#   列表非常适合用于存储在程序运行期间可能变化的数据集。列表是可以修改的，这对处理网站的用户列表或游戏中的角色列表至关重要。
+#   然而，有时候你需要创建一系列不可修改的元素，元组可以满足这种需求。Python将不能修改的值称为不可变的，而不可变的列表被称为元组。
+
+#   元组看起来犹如列表，但使用圆括号而不是方括号来标识。定义元组后，就可以使用索引来访问其元素，就像访问列表元素一样。
+#   例如，如果有一个大小不应改变的矩形，可将其长度和宽度存储在一个元组中，从而确保它们是不能修改的：
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+
+#   虽然不能修改元组的元素，但可以给存储元组的变量赋值。因此，如果要修改前述矩形的尺寸，可重新定义整个元组：
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+#   相比于列表，元组是更简单的数据结构。如果需要存储的一组值在程序的整个生命周期内都不变，可使用元组。
+
+#   编程时经常需要检查一系列条件，并据此决定采取什么措施。在Python中，if语句让你能够检查程序的当前状态，并据此采取相应的措施。
+
+#   每条if语句的核心都是一个值为True或False的表达式，这种表达式被称为条件测试。Python根据条件测试的值为True还是False来决定是否
+#   执行if语句中的代码。如果条件测试的值为True，Python就执行紧跟在if语句后面的代码；如果为False，Python就忽略这些代码。
+
+#   大多数条件测试都将一个变量的当前值同特定值进行比较。最简单的条件测试检查变量的值是否与特定值相等：
+
+
+#   检查是否相等时不考虑大小写
+
+#   你可能想同时检查多个条件，例如，有时候你需要在两个条件都为True时才执行相应的操作，
+#   而有时候你只要求一个条件为True时就执行相应的操作。在这些情况下，关键字and和or可助你一臂之力。
+#   1. 使用and检查多个条件
+#   要检查是否两个条件都为True，可使用关键字and将两个条件测试合而为一；如果每个测试都通过了，整个表达式就为True；如果至少有一个测试没有通过，整个表达式就为False。
+
+#   为改善可读性，可将每个测试都分别放在一对括号内，但并非必须这样做。
+#   2. 使用or检查多个条件
+#   关键字or也能够让你检查多个条件，但只要至少有一个条件满足，就能通过整个测试。仅当两个测试都没有通过时，使用or的表达式才为False。
+
+
+#   有时候，执行操作前必须检查列表是否包含特定的值。例如，结束用户的注册过程前，可能需要检查他提供的用户名是否已包含在用户名列表中。
+#   在地图程序中，可能需要检查用户提交的位置是否包含在已知位置列表中。要判断特定的值是否已包含在列表中，可使用关键字in。
+#   来看你可能为比萨店编写的一些代码；这些代码首先创建一个列表，其中包含用户点的比萨配料，然后检查特定的配料是否包含在该列表中。
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+
+
+#   为让代码更简洁，可不在if-elif-else代码块中打印门票价格，而只在其中设置门票价格，并在它后面添加一条简单的print语句：
+
+age = 120
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
+
+
+
+#   else是一条包罗万象的语句，只要不满足任何if或elif中的条件测试，其中的代码就会执行，这可能会引入无效甚至恶意的数据。如果知道最终要测试的条件，
+#   应考虑使用一个elif代码块来代替else代码块。这样，你就可以肯定，仅当满足相应的条件时，你的代码才会执行。
+
+
+#   if-elif-else结构功能强大，但仅适合用于只有一个条件满足的情况：遇到通过了的测试后，Python就跳过余下的测试。这种行为很好，效率很高，
+#   让你能够测试一个特定的条件。
+#   然而，有时候必须检查你关心的所有条件。在这种情况下，应使用一系列不包含elif和else代码块的简单if语句。在可能有多个条件为True，
+#   且你需要在每个条件为True时都采取相应措施时，适合使用这种方法。
+
+#   总之，如果你只想执行一个代码块，就使用if-elif-else结构；如果要运行多个代码块，就使用一系列独立的if语句。
+
+######################################################################################################################################################################################################
+#   姓名：将一些朋友的姓名存储在一个列表中，并将其命名为names。依次访问该列表中的每个元素，从而将每个朋友的姓名都打印出来。
+
+names = ['ZhanSan','LiSi','WangWu','HouLiu']
+
+#   方法一：
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+#   方法二：
+for i in range(0,4):
+    print(names[i])
+
+######################################################################################################################################################################################################
+
+
+
+
+######################################################################################################################################################################################################
+#   问候语：继续使用练习3-1 中的列表，但不打印每个朋友的姓名，而为每人打印一条消息。每条消息都包含相同的问候语，但抬头为相应朋友的姓名。
+
+names = ['ZhanSan','LiSi','WangWu','HouLiu']
+
+#   方法一：
+print('Goodmorning!Dear' + str(names[0]) + '!')
+print('Goodmorning!Dear' + str(names[1]) + '!')
+print('Goodmorning!Dear' + str(names[2]) + '!')
+print('Goodmorning!Dear' + str(names[3]) + '!')
+
+#   方法二：
+for i in range(0,4):
+    print('Goodmorning!Dear' + str(names[i]) + '!')
+
+######################################################################################################################################################################################################
+
+
+
+
+######################################################################################################################################################################################################
+#   自己的列表：想想你喜欢的通勤方式，如骑摩托车或开汽车，并创建一个包含多种通勤方式的列表。根据该列表打印一系列有关这些通勤方式的宣言，如“I would liketo own a Honda motorcycle”。
+
+Commuting_modes = ['Bus', 'Subway', 'Car', 'Bycicle']
+
+#   方法一：
+print('I would liketo own a ' + str(Commuting_modes[0]) + '!')
+print('I would liketo own a ' + str(Commuting_modes[1]) + '!')
+print('I would liketo own a ' + str(Commuting_modes[2]) + '!')
+print('I would liketo own a ' + str(Commuting_modes[3]) + '!')
+
+#   方法二：
+for i in range(0,4):
+    print('I would liketo own a ' + str(Commuting_modes[i]) + '!')
+
+######################################################################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
