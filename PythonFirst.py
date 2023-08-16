32282b4878e7147e1d1285c7e0fed9ad9933fa9c
Tom Tony <Tomason@5n5bg6.onmicrosoft.com>
2023-08-16 18:42:01 +0800

    2023年8月18日09:17:13

--- PythonFirst.py
+++ PythonFirst.py
@@ -1172,12 +1172,67 @@ while unprinted_designs:
     #模拟根据设计制作3D打印模型的过程
     print("Printing model: " + current_design)
     completed_models.append(current_design)
+
     # 显示打印好的所有模型
-    print("\nThe following models have been printed:")
+print("\nThe following models have been printed:")
 for completed_model in completed_models:
     print(completed_model)
 
 
+#   这个程序首先创建一个需要打印的设计列表，还创建一个名为completed_models的空列表，
+#   每个设计打印都将移到这个列表中。只要列表unprinted_designs中还有设计，while循环就
+#   模拟打印设计的过程：从该列表末尾删除一个设计，将其存储到变量current_design中，并
+#   显示一条消息，指出正在打印当前的设计，再将该设计加入到列表completed_models中。
+
+'''
+    为重新组织这些代码，我们可编写两个函数，每个都做一件具体的工作。大部分代码都与原来相同，只是效率更高。
+'''
+def print_models(unprinted_designs, completed_models):
+
+    #   模拟打印每个设计，直到没有未打印的设计为止
+    #   打印每个设计后，都将其移到列表completed_models中
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        
+        # 模拟根据设计制作3D打印模型的过程
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    
+    #   显示打印好的所有模型
+    print("\nThe following models have been printed:")
+
+    for completed_model in completed_models:
+        print(completed_model)
+    
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+#   首先，我们定义了函数print_models()，它包含两个形参：一个需要打印的设计列表和一个打印好的模型列表。
+#   给定这两个列表，这个函数模拟打印每个设计的过程：将设计逐个地从未打印的设计列表中取出， 并加入到打印好的模型列表中。接着， 我们定义了函数
+#   show_completed_models() ，它包含一个形参：打印好的模型列表。给定这个列表，函数show_completed_models()显示打印出来的每个模型的名称。
+#   这个程序的输出与未使用函数的版本相同，但组织更为有序。完成大部分工作的代码都移到了两个函数中，让主程序更容易理解
+
+
+'''
+        我们创建了一个未打印的设计列表，还创建了一个空列表，用于存储打印好的模型。接下来，
+    由于我们已经定义了两个函数，因此只需调用它们并传入正确的实参即可。我们调用
+    print_models()并向它传递两个列表；像预期的一样，print_models()模拟打印设计的过程。接
+    下来，我们调用show_completed_models()，并将打印好的模型列表传递给它，让其能够指出打印
+    了哪些模型。描述性的函数名让别人阅读这些代码时也能明白，虽然其中没有任何注释。
+    相比于没有使用函数的版本，这个程序更容易扩展和维护。如果以后需要打印其他设计，
+    只需再次调用print_models()即可。如果我们发现需要对打印代码进行修改，只需修改这些代码
+    一次，就能影响所有调用该函数的地方；与必须分别修改程序的多个地方相比，这种修改的效
+    率更高。
+    这个程序还演示了这样一种理念，即每个函数都应只负责一项具体的工作。第一个函数打印
+    每个设计，而第二个显示打印好的模型；这优于使用一个函数来完成两项工作。编写函数时，如
+    果你发现它执行的任务太多，请尝试将这些代码划分到两个函数中。别忘了，总是可以在一个函
+    数中调用另一个函数，这有助于将复杂的任务划分成一系列的步骤。
+'''
 
 ######################################################################################################################################################################################################
 
